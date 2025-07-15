f3c695628efa1fc680929b6550af648ff18bd707
MukeshR-prog <mukeshr1855@gmail.com>
2025-07-15 18:07:18 +0530

    feat: integrate Firebase authentication and user management- Added Firebase configuration and initialization in src/lib/firebase.js- Created an Axios instance for API calls in src/lib/axios.js- Implemented user authentication state management using Zustand in src/store/authStore.js- Developed user registration API endpoint in src/app/api/users/route.js- Updated dashboard page to redirect unauthenticated users to login- Created login page with Google authentication and user creation logic- Enhanced home page with a login button- Updated package.json to include axios, firebase, and zustand dependencies

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import Navbar from './components/navbar'
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import useAuthStore from "../../store/authStore";
+import Navbar from "./components/navbar";
 
 const Page = () => {
-  return (
-    <div className='h-full w-full flex '>
-        <div className='bg-red-500 w-[30%] h-screen'> <Navbar/></div>
-        <div className='bg-blue-600 w-[70%] h-screen'>asd</div>
-    </div>
-  )
-}
+  const { user, loading } = useAuthStore();
+  const router = useRouter();
 
-export default Page
+  useEffect(() => {
+    if (!loading && !user) {
+      router.push("/login");
+    }
+  }, [user, loading]);
+
+  if (loading) return <p>Loading...</p>;
+  if (!user) return null;
+  return (
+    <div className="h-full w-full flex ">
+      <div className="bg-red-500 w-[30%] h-screen">
+        {" "}
+        <Navbar />
+      </div>
+      <div className="bg-blue-600 w-[70%] h-screen">
+        <h1 className="text-2xl mb-2">Dashboard</h1>
+        <p>Welcome, {user.displayName}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
